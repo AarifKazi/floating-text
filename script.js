42190fd10372c342d89b1b47943672d6faf577fe
AarifKazi <Kaziaarif61@gmail.com>
2024-06-20 07:11:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+var z = document.querySelector(".activeTwo");
+z.addEventListener("click", function(){
+    z.style.backgroundColor = "rgba(0, 119, 255, 0.418)";
+})
+
+var x = document.querySelector(".header-9");
+var y = document.querySelector(".containe")
+x.style.color = "black";
+y.style.display = "none"
+x.addEventListener("click",function(){
+ y.style.display = "flex"   
+})
+
+var a = document.querySelector(".header-8");
+var b = document.querySelector(".containe-2")
+a.style.color = "black";
+b.style.display = "none"
+a.addEventListener("click",function(){
+ b.style.display = "flex"   
+})
+
+var c = document.querySelector(".header-7");
+var d = document.querySelector(".containe-3")
+c.style.color = "black";
+d.style.display = "none"
+c.addEventListener("click",function(){
+ d.style.display = "flex"   
+})
